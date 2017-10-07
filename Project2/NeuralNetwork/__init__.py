c3616a5b8ba0534a9d9a4fe8daf84a86c4601867
Na'Shea Wiesner <nashea.wiesner@centricient.com>
2017-10-07 15:25:34 -0600

    Better organization of files

---
+++ Project2/NeuralNetwork/__init__.py
@@ -0,0 +1,3 @@
+# handler for calling both networks
+# need code for inputting the csvs into the input_values and output_values arrays
+# code to call each network for all dimensions of rosenbrock with varying network topologies
---
+++ Project2/__init__.py
@@ -0,0 +1,2 @@
+# handler for calling both networks
+# need code for inputting the csvs into the input_values and output_values arrays
