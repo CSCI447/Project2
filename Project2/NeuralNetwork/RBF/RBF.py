e6bdb072b08f6c3d0f181778ec74d4cb32866a15
Na'Shea Wiesner <nashea.wiesner@centricient.com>
2017-10-07 17:23:16 -0600

    test/train split and calculated weighted sums

--- Project2/NeuralNetwork/RBF/RBF.py
+++ Project2/NeuralNetwork/RBF/RBF.py
@@ -13,10 +13,9 @@ from NeuralNetwork.RBF import K_Means
 class NN:
 
     def __init__(self, input_values, gaussian_amount, output_nodes_amount, expected_output_values, answers, learnrate = 0.3, threshold = 1, momentum = 0.5, maximum = 0, minimum = 1000):
-        self.input_values = input_values
+        self.training,self.testing = self.creat_io_pairs(input_values, expected_output_values)
         self.hidden_layers_amount = 1
         self.gaussian_amount = gaussian_amount
-        self.expected_output_values = expected_output_values
         self.output_nodes_amount = output_nodes_amount
         self.answerSet = answers
         self.learnRate = learnrate
@@ -27,15 +26,29 @@ class NN:
         self.inputNodes = []
         self.hiddenNodes = []
         self.outputNodes = []
-        self.centroids = self.get_centroids(self.input_values, self.gaussian_amount)
+        self.centroids = self.get_centroids(input_values, self.gaussian_amount)
         self.converged = False
         self.batches = self.form_batches()
         self.beta = 1.0
+        self.network = self.build_network(self)
+        self.weighted_sum = 0
+        self.squared_error = 0
 
         # Initialize a network
 
+    def create_io_pairs(self,input,output):
+        partition = len(input) * 0.8
+        for i in len(input()):
+            if i < partition:
+                ex = Example(input[i],output[i])
+                self.training.append(ex)
+            elif i >= partition:
+                ex = Example(input[i], output[i])
+                self.testing.append(ex)
+        return self.training, self.testing
+
     def build_network(self):
-        for x in self.input_values:
+        for x in self.training:
             n = Neuron()
             n.setValue(x)
             self.inputNodes.append(n)
@@ -47,6 +60,7 @@ class NN:
         for x in range(self.output_nodes_amount):
             n = Neuron()
             self.outputNodes.append(n)
+        self.connect_network(self)
 
     def connect_network(self):
         # for x in self.inputNodes:
@@ -72,6 +86,7 @@ class NN:
             neuron.setConnections(connections)
             # neuron.setInputNodes(self.hiddenNodes)
             # neuron.setNodeWeightsLength(len(neuron.getInputNodes))
+        self.initialize_weights(self)
 
     def initialize_weights(self):
         #for neuron in self.hiddenNodes:    #there's only one set of weights in RBF's between the hidden layer and the output
@@ -88,10 +103,10 @@ class NN:
     def form_batches(self):
         batch_size = 10
         index = 0
-        for i in range(len(self.input_values)/batch_size):
+        for i in range(len(self.training)/batch_size):
             batch = []
             for j in range(batch_size):
-                batch.append(self.input_values[index])
+                batch.append(self.training[index])
             self.batches.append(batch)
         return self.batches
 
@@ -101,6 +116,11 @@ class NN:
                 for n in range(self.gaussian_amount):
                     value = self.apply_gaussian(self.batches[i][j],self.centroids[n])
                     self.hiddenNodes[n].setValue(value)
+                self.calculate_weighted_sum(self)
+                while (self.calculate_error(self) > self.threshold):
+                    self.update_weights()
+                print('Predicted = ' + self.weighted_sum)
+                print('Actual = ' + self.batches[i][j].output)
 
     def calculate_distance(self,x,mu):
         return linalg.norm(x-mu)
@@ -110,10 +130,22 @@ class NN:
         return phi
 
     def calculate_weighted_sum(self):
-        return
+        for i in self.hiddenNodes:
+            value = self.hiddenNodes[i].value
+            for j in self.outputNodes:
+                self.weighted_sum += (value * self.hiddenNodes[i].connections[j].weight)
+        return self.weighted_sum
 
     def calculate_error(self):
-        return
+        #calculate squared error
+        return self.squared_error
 
     def update_weights(self):
+        #update weights
+        self.weighted_sum = self.calculate_weighted_sum(self)
         return
+
+class Example():
+    def __init__(self, input, output):
+        self.input = input
+        self.out = output
